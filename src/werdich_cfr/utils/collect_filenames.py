ff65d359fd95f9f3b7a2e7eb05757eaa906488a4
awerdich <awerdich@gmail.com>
2020-01-29 16:28:45 -0500

    collect and combine npy and meta data.

--- src/werdich_cfr/utils/collect_filenames.py
+++ src/werdich_cfr/utils/collect_filenames.py
@@ -2,14 +2,20 @@
 import os
 import glob
 import numpy as np
+from Crypto.Cipher import AES
 import pandas as pd
 import time
 
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 50)
+pd.set_option('display.width', 500)
 
-#%% Files and folders
+#%% Files, directories and parameters
 cfr_data_root = os.path.normpath('/mnt/obi0/andreas/data/cfr')
 cfr_echo_dir = os.path.normpath('/mnt/obi0/phi/echo/npyFiles/BWH')
-#test_dir = os.path.join(cfr_echo_dir, '48bd')
+cfr_feather_dir = os.path.normpath('/mnt/obi0/phi/echo/featherFiles/BWH')
+key='rahuldeoechobwh*'
+iv='echoisexcellent*'
 
 def collect_files(topdir):
 
@@ -31,7 +37,63 @@ def collect_files(topdir):
 
     return df
 
-#%% Save as pd data frame
-df = collect_files(cfr_echo_dir)
-file_list_name = 'echo_npyFiles_BWH.parquet'
-df.to_parquet(os.path.join(cfr_data_root, file_list_name))
+def decodeMRN(MRN, key, iv):
+    decipher = AES.new(key, AES.MODE_CFB, iv)
+    MRN=bytes.fromhex(MRN)
+    oldmrn = decipher.decrypt(MRN).decode()
+    # oldmrn = unhexlify(MRN)
+    return oldmrn
+
+def decode_file(filename):
+    """ Decode a filename in [mnr, datetime] """
+    base_name_list = [filename.split('_')[i] for i in range(2)]
+    study = base_name_list[0] + '_' + base_name_list[1]
+    mrn_date = [decodeMRN(encoded, key = key, iv = iv) for encoded in base_name_list]
+    return study, mrn_date
+
+def add_base_name_mrn_datetime(df):
+
+    df = df.assign(study=df.filename.apply(lambda s: decode_file(s)[0]),
+                   mrn=df.filename.apply(lambda s: decode_file(s)[1][0]),
+                   datetime=df.filename.apply(lambda s: decode_file(s)[1][1]))
+
+    df.datetime = pd.to_datetime(df.datetime, infer_datetime_format=True)
+
+    return df
+
+#%% Load the file name lists
+
+npy_file_list_name = 'echo_npyFiles_BWH.parquet'
+df_npy_file = collect_files(cfr_echo_dir)
+df_npy_file_2 = add_base_name_mrn_datetime(df_npy_file)
+df_npy_file_2.to_parquet(os.path.join(cfr_data_root, npy_file_list_name))
+
+feather_file_list_name = 'echo_featherFiles_BWH.parquet'
+df_feather_file = collect_files(cfr_feather_dir)
+df_feather_file_2 = add_base_name_mrn_datetime(df_feather_file)
+df_feather_file_3 = df_feather_file_2.assign(dsc = df_feather_file_2.filename.apply(
+    lambda s: os.path.splitext(s)[0].replace(decode_file(s)[0], '')[1:]))
+df_feather_file_3.to_parquet(os.path.join(cfr_data_root, feather_file_list_name))
+
+df_feather_file_3 = df_feather_file_3.rename(columns = {'filename': 'meta_filename',
+                                                        'dir': 'meta_dir'})
+df_files = df_npy_file_2.merge(right = df_feather_file_3, how = 'left', on = ['study', 'mrn', 'datetime'])
+
+# Join npy_file_list and feather_file_list
+# Rename some of the feather data columns
+npy_meta_name = 'echo_BWH_npy_feather_files.parquet'
+df_files.to_parquet(os.path.join(cfr_data_root, npy_meta_name))
+
+#df_npy_file_2 = pd.read_parquet(os.path.join(cfr_data_root, npy_file_list_name))
+#df_feather_file_3 = pd.read_parquet(os.path.join(cfr_data_root, feather_file_list_name))
+
+#%% Look at the meta data
+npy_meta_name = 'echo_BWH_npy_feather_files.parquet'
+df_files = pd.read_parquet(os.path.join(cfr_data_root, npy_meta_name))
+
+# Pick a study
+study = list(df_files.study.unique())[10]
+df = df_files[df_files.study == study]
+
+
+
