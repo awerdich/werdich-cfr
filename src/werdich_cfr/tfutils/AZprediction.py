4fbc67849319a74c6c760ee8f3e7a54e24c55e16
awerdich <awerdich@gmail.com>
2020-11-15 08:26:18 -0500

    Simplified prediction script.

--- src/werdich_cfr/tfutils/AZprediction.py
+++ src/werdich_cfr/tfutils/AZprediction.py
@@ -37,8 +37,20 @@ def get_im_generator(im_array_list):
             yield (element[0], element[1])
     return im_generator
 
-def predict_from_array_list(model, array_list, batch_size):
+def predict_from_array_list(model, model_dict, feature_dict, array_list, batch_size):
+    '''
+    Predict from list of echo videos
+    model: compiled (or loaded from checkpoint) keras model
+    model_dict: dict with model hyperparameters
+    feture_dict: names of input and output tensors
+    array_list: list of np.ndarray videos
+    '''
+
     im_generator = get_im_generator(array_list)
+
+    dsp = DatasetProvider(augment=False,
+                          im_scale_factor=model_dict['im_scale_factor'],
+                          feature_dict=feature_dict)
 
     dset = tf.data.Dataset.from_generator(generator=im_generator,
                                           output_types=(tf.int32, tf.int32),
@@ -66,6 +78,7 @@ batch_size = 8
 # Directory for metadata, predictions
 cfr_project_dir = os.path.normpath('/mnt/obi0/andreas/data/cfr_AZ')
 predict_dir = os.path.join(cfr_project_dir, 'predictions')
+meta_date = '200617'
 
 # Model names and checkpoint files
 model_dir = os.path.join(cfr_project_dir, 'models')
@@ -79,8 +92,11 @@ video_meta_file = os.path.join(cfr_project_dir, video_meta_file_name)
 video_meta_df = pd.read_parquet(os.path.join(video_meta_file))
 file_list = list(video_meta_df.filename.unique())
 
-#meta_date = '200617'
-#meta_dir = os.path.join(cfr_data_root, 'metadata_'+meta_date)
+# Features for the model
+feature_dict_name = 'feature_dict_' + 'tfr_' + meta_date+'.pkl'
+feature_dict_file = os.path.join(model_dir, feature_dict_name)
+with open(feature_dict_file, 'rb') as fl:
+    feature_dict = pickle.load(fl)
 
 #%% Load video data into memory and start preprocessing
 print(f'Loading {len(file_list)} echos into memory from file: {os.path.basename(video_meta_file_name)}.')
@@ -104,56 +120,47 @@ for f, filename in enumerate(file_list):
         image_array_list.append((im, np.asarray(im.shape, np.int32)))
         image_array_file_list.append(filename)
     else:
-        echo_df_fl = video_meta_df[video_meta_df.filename == filename].assign(err=[error])
-        meta_disqualified_list.append(echo_df_fl)
+        meta_disqualified_list.append(video_meta_df[video_meta_df.filename == filename].\
+                                      assign(err=[error]))
         print('Skipping this one.')
 
 if len(meta_disqualified_list) > 0:
     echo_df_disqualified = pd.concat(meta_disqualified_list, ignore_index=True)
     # Save disqualified metadata
     print(f'Found {echo_df_disqualified.shape[0]} of {len(file_list)} disqualified videos.')
-    disqualified_filename = os.path.basename(video_meta_df).split('.')[0] + '_disqualified.parquet'
-    echo_df_disqualified.to_parquet(os.path.join(predict_dir, disqualified_filename))
+    disqualified_filename = os.path.basename(video_meta_file_name).split('.')[0] + '_disqualified.parquet'
+    echo_df_disqualified.to_parquet(os.path.join(cfr_project_dir, disqualified_filename))
 
 print(f'Loaded {len(image_array_list)} of {len(file_list)} videos into memory.')
-
 
 #%% Run predictions for all models
 
 # Loop over the models
 for m, model_name in enumerate(model_list):
 
-    print(f'Loading model {m+1}/{len(model_list)}: {model_name}')
-
+    # Model information from this series
     model_s = checkpoint_df[checkpoint_df.model_name == model_name].iloc[0]
     dset = model_s.dset
-    model_name = model_s.model_name
-    tfr_dir = os.path.join(cfr_data_root, 'tfr_'+meta_date, dset)
-    log_dir = os.path.join(cfr_data_root, 'log', model_name)
-    checkpoint_file = model_s.checkpoint_file
 
-    print('Loading model from checkpoint {}.'.format(os.path.basename(checkpoint_file)))
+    # Load model from checkpoint
+    checkpoint_file_name = os.path.basename(model_s.checkpoint_file)
+    checkpoint_file = os.path.join(model_dir, checkpoint_file_name)
+    print('Loading model from checkpoint: {}.'.format(os.path.basename(checkpoint_file)))
     model = load_model(checkpoint_file)
+    model.summary()
 
-    # We need the model_dict for the correct image transformations
-    model_dict_file = os.path.join(log_dir, model_name+'_model_dict.pkl')
+    # Model hyperparameters and output name
+    model_dict_file_name = model_name + '_model_dict.pkl'
+    model_dict_file = os.path.join(model_dir, model_dict_file_name)
     with open(model_dict_file, 'rb') as fl:
         model_dict = pickle.load(fl)
     model_output = model_dict['model_output']
 
-    # feature_dict
-    feature_dict_file = glob.glob(os.path.join(tfr_dir, '*.pkl'))[0]
-    with open(feature_dict_file, 'rb') as fl:
-        feature_dict = pickle.load(fl)
-
-    dsp = DatasetProvider(augment=False,
-                          im_scale_factor=model_dict['im_scale_factor'],
-                          feature_dict=feature_dict)
-
     # Predict from image_array_list
-
     n_steps = int(np.ceil(len(image_array_list) / batch_size))
     predictions = predict_from_array_list(model=model,
+                                          model_dict=model_dict,
+                                          feature_dict=feature_dict,
                                           array_list=image_array_list,
                                           batch_size=batch_size)
 
@@ -165,9 +172,9 @@ for m, model_name in enumerate(model_list):
                               'checkpoint': [os.path.basename(checkpoint_file)]*len(predictions)})
 
     # Merge output predictions and save data
-    pred_df = pred_df_c.merge(right=echo_df, on='filename', how='left').reset_index(drop=True)
+    pred_df = pred_df_c.merge(right=video_meta_df, on='filename', how='left').reset_index(drop=True)
 
-    video_list_file_name = model_name+'.parquet'
-    video_list_file = os.path.join(predict_dir, 'cfr_models_30fps', video_list_file_name)
+    video_list_file_name = video_meta_file_name.split('.')[0]+'_'+dset+'_'+model_output+'.parquet'
+    video_list_file = os.path.join(predict_dir, video_list_file_name)
 
     pred_df.to_parquet(video_list_file)
